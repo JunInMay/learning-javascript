9c241a23e65a91924fb2aea409275d54e4e83972
Chana <twszak01@gmail.com>
2024-10-20 23:04:33 +0900

    [2024-10-20] 원시값의 메서드 공부 1

---
+++ modern javascript tutorial/5.1. Methods of primitives/methodsOfPrimitives1.js
@@ -0,0 +1,7 @@
+/*
+2024-10-20 원시값의 메서드 공부 1
+*/
+let str = "Hello..? I'm... ";
+
+console.log(str.toUpperCase()); // HELLO..? I'M... 
+console.log(str); // Hello..? I'm... 
---
+++ modern javascript tutorial/5.1. Methods of primitives/methodsOfPrimitives2.js
@@ -0,0 +1,11 @@
+/*
+2024-10-20 원시값의 메서드 공부 2
+*/
+
+let number1 = 1.4321;
+let number2 = 1.6543;
+
+console.log(number1.toFixed(1)); // 1.4
+console.log(number2.toFixed(1)); // 1.7
+console.log(number1);
+console.log(number2);
